f67ac3607c6d7d7ac48e08e4b5e3324f57145503
maaz khan <maazkhaansofteng@gmail.com>
2024-07-14 12:10:13 +0500

    new changes

--- utils/dbUtils.js
+++ utils/dbUtils.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose');
 
 async function connectToDatabase() {
+    const dbURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/studentDB'; 
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/studentDB', {
-            // useNewUrlParser: true,
-            // useUnifiedTopology: true
-        });
+        await mongoose.connect(dbURI);
         console.log('Connected to MongoDB');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error);
+        process.exit(1); 
     }
 }
 
