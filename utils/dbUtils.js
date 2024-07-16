45283fede8d67020b25b96f3de1177b5e7501386
maaz khan <maazkhaansofteng@gmail.com>
2024-07-16 15:12:53 +0500

    db chages

--- utils/dbUtils.js
+++ utils/dbUtils.js
@@ -1,20 +1,27 @@
-const mongoose = require('mongoose');
+
 require('dotenv').config();
+const mongoose = require('mongoose');
 
 async function connectToDatabase() {
-    const dbURI = process.env.MONGODB_URI;
+    const dbURI = process.env.MONGODB_URI; 
+    if (!dbURI) {
+        console.error('Error: MONGODB_URI is not defined');
+        process.exit(1); // Exit the process if the URI is not defined
+    }
+
     try {
         await mongoose.connect(dbURI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 5000, // Adjust the timeout value as needed
+            socketTimeoutMS: 45000, // Adjust the timeout value as needed
         });
         console.log('Connected to MongoDB');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error);
-        process.exit(1);
+        process.exit(1); 
     }
 
-    // Additional event logging
     mongoose.connection.on('connected', () => {
         console.log('Mongoose connected to ' + dbURI);
     });
